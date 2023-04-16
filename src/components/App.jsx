d50c4cbf8d2ef9cb265754c6fecfc06b1cf1a931
Alex-Fedkov <107791953+Alex-Fedkov@users.noreply.github.com>
2023-04-16 17:24:47 +0300

    hw2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,18 +21,6 @@ export const App = () => {
   const [number, setNumber] = useState("");
   const [filter, setFilter] = useState("");
 
-  // useEffect(() => {
-  //   console.log("component will mount");
-  //   const loadedContacts = loadKey(LOCALSTORAGE_CONTACTS_KEY) || [
-  //     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  //     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  //     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  //     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  //   ];
-  //   console.log("loadedContacts", loadedContacts)
-  //   setContacts(loadedContacts);
-  // }, []);
-
   useEffect(() => {
     saveKey(LOCALSTORAGE_CONTACTS_KEY, contacts);
   }, [contacts]);
